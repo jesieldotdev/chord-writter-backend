05c4024f03dd9f90c7a89987f8325f20c1d0e659
jesieldotdev <jesiel364@gmail.com>
2024-01-06 20:17:25 -0400

    change the name

--- server.ts
+++ server.ts
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 
 const server = http.createServer(app);
 
-const port = process.env.PORT ?? 4000;
+const port = process.env.PORT ?? 4001;
 
 const getMusics = async () => {
   try {
